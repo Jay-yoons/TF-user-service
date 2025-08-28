e010437cf36d6b162c7037ac9b7bdae90c4af35f
KHP-SOLDESK1-03\SD1-03 <asanyong1@naver.com>
2025-08-28 09:43:24 +0900

    Fix JPA EntityManager issue and optimize production settings

--- src/main/java/com/restaurant/reservation/UserServiceApplication.java
+++ src/main/java/com/restaurant/reservation/UserServiceApplication.java
@@ -2,12 +2,16 @@ package com.restaurant.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import jakarta.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.restaurant.reservation.repository")
+@EntityScan(basePackages = "com.restaurant.reservation.entity")
 @EnableScheduling
 public class UserServiceApplication {
 
